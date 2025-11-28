de79c3af9af9caa6b95596c89d809f2b15ade9c1
pablo <taddeiApablo23@gmail.com>
2025-11-28 10:55:55 -0300

    comenzando a armar el saas
    comenzando a armar el saas

--- Code_Health_Audit_Project/CodeHealthAudit_Webapp/routes/index.js
+++ Code_Health_Audit_Project/CodeHealthAudit_Webapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Aqui el Home de la app' });
 });
 
 module.exports = router;
--- Code_Health_Audit_Project/CodeHealthAudit_Webapp/routes/users.js
+++ Code_Health_Audit_Project/CodeHealthAudit_Webapp/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/Login', function(req, res, next){
+  res.render('login');
+});
+
+router.get('/Sign-up', function(req, res, next){ 
+  res.render('singup');
+});
+
 module.exports = router;
